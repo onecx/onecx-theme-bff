4de257454fd1c20a099b671a5c236d37397cc8e7
Jorden_Reuter <jorden.reuter@capgemini.com>
2023-11-21 16:42:39 +0100

    feat: clean-up

--- src/main/java/io/github/onecx/themes/bff/rs/ThemeRestController.java
+++ src/main/java/io/github/onecx/themes/bff/rs/ThemeRestController.java
@@ -30,8 +30,6 @@ public class ThemeRestController implements ThemesApiService {
     ThemeMapper mapper;
 
     @Inject
-    ObjectMapper objectMapper;
-    @Inject
     ResponseMapper responseMapper;
 
     @Override
--- src/main/java/io/github/onecx/themes/bff/rs/mappers/RequestMapper.java
+++
@@ -1,9 +0,0 @@
-package io.github.onecx.themes.bff.rs.mappers;
-
-import org.mapstruct.Mapper;
-import org.tkit.quarkus.rs.mappers.OffsetDateTimeMapper;
-
-@Mapper(uses = { OffsetDateTimeMapper.class })
-public class RequestMapper {
-
-}
