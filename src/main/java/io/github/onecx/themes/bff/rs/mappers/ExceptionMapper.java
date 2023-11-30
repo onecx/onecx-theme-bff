3e45885426b096efd29e1f0b653ee7d0b5a299fa
Jorden_Reuter <jorden.reuter@capgemini.com>
2023-11-30 09:25:15 +0100

    fix: formatting

--- src/main/java/io/github/onecx/themes/bff/rs/mappers/ExceptionMapper.java
+++ src/main/java/io/github/onecx/themes/bff/rs/mappers/ExceptionMapper.java
@@ -18,43 +18,43 @@ import gen.io.github.onecx.theme.bff.rs.internal.model.ProblemDetailInvalidParam
 import gen.io.github.onecx.theme.bff.rs.internal.model.ProblemDetailParamDTO;
 import gen.io.github.onecx.theme.bff.rs.internal.model.ProblemDetailResponseDTO;
 
-@Mapper(uses = {OffsetDateTimeMapper.class})
+@Mapper(uses = { OffsetDateTimeMapper.class })
 public abstract class ExceptionMapper {
 
-  public RestResponse<ProblemDetailResponseDTO> constraint(ConstraintViolationException ex) {
-    var dto = exception("CONSTRAINT_VIOLATIONS", ex.getMessage());
-    dto.setInvalidParams(createErrorValidationResponse(ex.getConstraintViolations()));
-    return RestResponse.status(Response.Status.BAD_REQUEST, dto);
-  }
-
-  @Mapping(target = "removeParamsItem", ignore = true)
-  @Mapping(target = "params", ignore = true)
-  @Mapping(target = "invalidParams", ignore = true)
-  @Mapping(target = "removeInvalidParamsItem", ignore = true)
-  public abstract ProblemDetailResponseDTO exception(String errorCode, String detail);
-
-  public List<ProblemDetailParamDTO> map(Map<String, Object> params) {
-    if (params == null) {
-      return List.of();
+    public RestResponse<ProblemDetailResponseDTO> constraint(ConstraintViolationException ex) {
+        var dto = exception("CONSTRAINT_VIOLATIONS", ex.getMessage());
+        dto.setInvalidParams(createErrorValidationResponse(ex.getConstraintViolations()));
+        return RestResponse.status(Response.Status.BAD_REQUEST, dto);
     }
-    return params.entrySet().stream().map(e -> {
-      var item = new ProblemDetailParamDTO();
-      item.setKey(e.getKey());
-      if (e.getValue() != null) {
-        item.setValue(e.getValue().toString());
-      }
-      return item;
-    }).toList();
-  }
 
-  public abstract List<ProblemDetailInvalidParamDTO> createErrorValidationResponse(
-          Set<ConstraintViolation<?>> constraintViolation);
+    @Mapping(target = "removeParamsItem", ignore = true)
+    @Mapping(target = "params", ignore = true)
+    @Mapping(target = "invalidParams", ignore = true)
+    @Mapping(target = "removeInvalidParamsItem", ignore = true)
+    public abstract ProblemDetailResponseDTO exception(String errorCode, String detail);
 
-  @Mapping(target = "name", source = "propertyPath")
-  @Mapping(target = "message", source = "message")
-  public abstract ProblemDetailInvalidParamDTO createError(ConstraintViolation<?> constraintViolation);
+    public List<ProblemDetailParamDTO> map(Map<String, Object> params) {
+        if (params == null) {
+            return List.of();
+        }
+        return params.entrySet().stream().map(e -> {
+            var item = new ProblemDetailParamDTO();
+            item.setKey(e.getKey());
+            if (e.getValue() != null) {
+                item.setValue(e.getValue().toString());
+            }
+            return item;
+        }).toList();
+    }
 
-  public String mapPath(Path path) {
-    return path.toString();
-  }
+    public abstract List<ProblemDetailInvalidParamDTO> createErrorValidationResponse(
+            Set<ConstraintViolation<?>> constraintViolation);
+
+    @Mapping(target = "name", source = "propertyPath")
+    @Mapping(target = "message", source = "message")
+    public abstract ProblemDetailInvalidParamDTO createError(ConstraintViolation<?> constraintViolation);
+
+    public String mapPath(Path path) {
+        return path.toString();
+    }
 }
