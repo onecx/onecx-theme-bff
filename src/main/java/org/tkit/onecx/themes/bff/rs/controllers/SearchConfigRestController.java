3bad581aea5086cf79ffd952eb471d23fa56cc1c
Bastian Jakobi <bastian.jakobi@capgemini.com>
2024-03-05 12:28:36 +0100

    fix: update imports

--- src/main/java/org/tkit/onecx/themes/bff/rs/controllers/SearchConfigRestController.java
+++ src/main/java/org/tkit/onecx/themes/bff/rs/controllers/SearchConfigRestController.java
@@ -1,18 +1,19 @@
 package org.tkit.onecx.themes.bff.rs.controllers;
 
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.Response;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
 import gen.org.tkit.onecx.searchconfig.v1.client.api.SearchConfigApi;
 import gen.org.tkit.onecx.theme.bff.rs.internal.SearchConfigApiService;
 import gen.org.tkit.onecx.theme.bff.rs.internal.model.CreateSearchConfigRequestDTO;
 import gen.org.tkit.onecx.theme.bff.rs.internal.model.UpdateSearchConfigRequestDTO;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.core.Response;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 public class SearchConfigRestController implements SearchConfigApiService {
     @Inject()
     @RestClient()
     SearchConfigApi searchConfigApi;
-
 
     @Override
     public Response createSearchConfig(CreateSearchConfigRequestDTO createSearchConfigRequestDTO) {
